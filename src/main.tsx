22995e3b6d8144226d8def63697f1b6e100e53af
Bert W Summers <summersb@moesol.com>
2025-04-06 21:56:34 -0700

    added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import './index.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
@@ -43,7 +43,7 @@ if (rootElement && !rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient} >
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
       </QueryClientProvider>
     </StrictMode>,
   )
--- src/routes/todos/index.tsx
+++ src/routes/todos/index.tsx
@@ -1,10 +1,17 @@
+import { queryClient } from '@/main'
 import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/todos/')({
   component: RouteComponent,
-  loader: async (): Promise<Todo[]> =>
-      fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json()),
+  loader: async () => {
+    const queryKey = ['todos']
+    const queryFn = async (): Promise<Todo[]> => fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+    const todo = await queryClient.ensureQueryData({
+      queryKey, queryFn
+    })
+    return { todo, queryKey }
+  },
   pendingComponent: () => <>Loading ...</>,
   onError: (err) => {
     <div>Loading problem {JSON.stringify(err)}</div>
@@ -19,11 +26,11 @@ type Todo = {
 }
 
 function RouteComponent() {
-  const posts = Route.useLoaderData()
+  const {todo} = Route.useLoaderData()
 
   return (<div>
-    {posts.map(p => (
-      <div key={p.id}>
+    {todo.map(p => (
+      <div key={p.id} className='p-4 border-blue-500 border-2'>
         <Link to="/todos/$todoId" params={{ todoId: String(p.id) }}>
           {p.title}
         </Link>
