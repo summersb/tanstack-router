b55362bcfdc63d4d63222de5ff584360271bbe45
Bert W Summers <summersb@moesol.com>
2025-04-13 00:31:54 -0700

    switch todo to use useQuery

--- src/routes/todos/$todoId.tsx
+++ src/routes/todos/$todoId.tsx
@@ -1,16 +1,27 @@
 import {createFileRoute} from '@tanstack/react-router'
 import TodoCard from "@/components/todo/TodoCard.tsx";
+import type {Todo} from "@/types";
+import queryClient from "@/queryClient.ts";
 
 export const Route = createFileRoute('/todos/$todoId')({
   component: Todo,
-  loader: async ({params: {todoId}}) =>
-    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
-      .then(res => res.json()),
+  loader: async ({params: {todoId}}) => {
+    const queryKey = ['todos', todoId]
+    const queryFn = async (): Promise<Todo> =>
+      fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`).then(
+        (response) => response.json()
+      )
+    const todo = await queryClient.ensureQueryData({
+      queryKey,
+      queryFn,
+    })
+    return {todo, queryKey}
+  },
   pendingComponent: () => <>Loading...</>
 })
 
 function Todo() {
-  const todo = Route.useLoaderData()
+  const {todo} = Route.useLoaderData()
 
   return (
     <div>
